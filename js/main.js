545c38b9d2f197af5f4bbf1d27e9b6d8b3b37cf8
A.Immink <203846744@student.rocmondriaan.nl>
2021-03-24 16:27:12 +0100

    added bootstrap

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const button = document.querySelector(".theme-changer");
 button.addEventListener("click", function(){
-    const elements = document.querySelectorAll(".theme");
+    const elements = document.querySelectorAll(".theme");   
 
 for (let i = 0; i< elements.length; i++) { 
     const element= elements[i];
