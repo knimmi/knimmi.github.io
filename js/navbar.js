ceefe9ab7587bf6f7b5f6e4910be44f71455b673
Alex <you@example.com>
2021-06-28 08:36:13 +0200

    oude navbar verwijderd

--- js/navbar.js
+++
@@ -1,5 +0,0 @@
-    $(".menu-icon").click(function(){
-      $(this).toggleClass("active");
-      $(".navigation-menu").toggleClass("active");
-      $(".menu-icon i").toggleClass("fa-times");
-    });
