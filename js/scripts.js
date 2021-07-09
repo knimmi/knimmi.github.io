9ef12e73c7465319e079cf66892204faaa0e5eb0
Alex <you@example.com>
2021-07-09 10:56:51 +0200

    removed old portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  window.onscroll = function () {
-    if (this.scrollY > 20) {
+  document.addEventListener("DOMContentLoaded",function(){
+  window.onscroll = (function(){
+    if(this.scrollY > 20){
       $(".navbar").addClass("sticky");
       $(".goTop").fadeIn();
-    } else {
+    }
+    else{
       $(".navbar").removeClass("sticky");
       $(".goTop").fadeOut();
     }
-  };
-
-  $(".goTop").click(function () {
-    scroll(0, 0);
   });
 
-  $(".menu-toggler").click(function () {
+    $(".goTop").click(function(){scroll(0,0)});
+
+    $('.menu-toggler').click(function(){
     $(this).toggleClass("active");
     $(".navbar-menu").toggleClass("active");
   });
 
-  //thema switcher
+    //thema switcher
   const button = document.querySelector(".theme-changer");
-  button.addEventListener("click", function () {
-    const elements = document.querySelectorAll(".theme");
+  button.addEventListener("click", function(){
+    const elements = document.querySelectorAll(".theme");   
 
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
-      element.classList.toggle("dark1");
-    }
-  });
+  for (let i = 0; i< elements.length; i++) { 
+    const element= elements[i];
+    element.classList.toggle("dark1");
+}}) 
 });
+
